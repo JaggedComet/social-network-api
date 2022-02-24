c07e26a47134cb86c86daa7ffc9b6ee8b026d35c
Marco Flores <marcoflores1029@gmail.com>
2022-02-23 19:13:52 -0800

    fixed up user.js in models and lowercased the file names

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./reaction');
+const thoughtsSchema = require('./thoughts');
+
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+      },
+    },
+    thoughts: [thoughtsSchema],
+    friends: [userSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = Student;
