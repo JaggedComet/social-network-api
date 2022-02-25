618c98ca30cb22537e89025daa66a4ea99c381cf
Marco Flores <marcoflores1029@gmail.com>
2022-02-24 20:42:53 -0800

    added some stuff to index.js (server)

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,16 @@
 const { User, Thought } = require("../models");
+
+module.exports = {
+    //Get all Users
+    getUsers(req, res) {
+        User.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json(err));
+    },
+    getSingleUser(req, res) {
+        User.findOne({
+            _id: req.params.userId
+        }).then()
+    }
+
+}
