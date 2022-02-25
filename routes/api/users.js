0132fe72f3a83d51e206ba1120ec32dd35b5763e
Marco Flores <marcoflores1029@gmail.com>
2022-02-24 19:09:45 -0800

    finished user.js in api route?

---
+++ routes/api/users.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { route } = require('express/lib/application');
+
+const{
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+} = require("../../controllers/user-controller");
+
+// /api/users
+router.route('/')
+.get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId')
+.get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends
+router.route('/:userId/friends/:friendId')
+.post(addFriend).delete(deleteFriend);
+
+
+module.exports = router;
