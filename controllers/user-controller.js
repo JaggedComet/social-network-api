1e5ebdeeb45f164efe56544155b5cc61753dc3ac
Marco Flores <marcoflores1029@gmail.com>
2022-02-25 18:58:18 -0800

    finished add friend and delete friend in user controller I think?

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,9 +9,7 @@ module.exports = {
     },
     // Single User Get 
     getSingleUser(req, res) {
-        User.findOne({
-            _id: req.params.userId
-        }).then((user) => 
+        User.findOne({_id: req.params.userId}).then((user) => 
         !user
             ? res.status(404).json({message: "This ID doesn't exist"})
             : res.json(user)
@@ -28,7 +26,35 @@ module.exports = {
         .then((user) => 
             !user
                 ? res.status(404).json({message: "No user with this ID!"})
-                : res.json(course)
+                : res.json(user)
         ).catch((err) => res.status(500).json(err));
     },
+    // Deletes user by their _id
+    deleteUser(req, res) {
+        User.findOneAndDelete({_id: req.params.userId}).then((user) =>
+            !user
+                ? res.status(404).json({message: "No user with this ID!"})
+                : Thought.deleteMany({_id: {$in: user.thoughts}})
+        )
+        .then(() => res.json({message: "User along with their thoughts are deleted!"}))
+        .catch((err) => res.status(500).json(err));
+    },
+    // Add a friend to the User friend array
+    addFriend(req, res) {
+        User.findOneAndUpdate({_id: req.params.userId}, {$addToSet: {friends: req.params.friendId}}, {new: true})
+        .then((user) => 
+            !user
+                ? res.status(404).json({message: "No friend with this ID!"})
+                : res.json(user)
+        ).catch((err) => res.status(500).json(err));
+    },
+    // Remove friend from User Friend Array
+    deleteFriend(req, req) {
+        User.findOneAndUpdate({_id: req.params.userId}, {$pull: {friends: req.params.friendId}}, {new: true})
+        .then((user) => 
+        !user
+            ? res.status(404).json({message: "No friend with this ID!"})
+            : res.json(user)
+        ).catch((err) => res.status(500).json(err));
+    }
 }
